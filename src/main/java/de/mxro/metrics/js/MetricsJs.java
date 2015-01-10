c1cb2f1dbb4e3a604ce6625d5f17e8df2ed7c286
mxro <mxro@nowhere.com>
2015-01-10 11:45:12 +1100

    Adding services for monitoring services Autocommit Sat Jan 10 11:45:12 EST 2015

--- src/main/java/de/mxro/metrics/js/MetricsJs.java
+++ src/main/java/de/mxro/metrics/js/MetricsJs.java
@@ -4,16 +4,17 @@ import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.ExportPackage;
 import org.timepedia.exporter.client.Exportable;
 
+import de.mxro.metrics.js.internal.JsMetricsNode;
+
 @ExportPackage("")
 @Export("Nextweb")
 public class MetricsJs implements Exportable {
 
-    
     @Export
     public static JsMetricsNode create() {
-        
+
     }
-    
+
     public MetricsJs() {
         super();
     }
