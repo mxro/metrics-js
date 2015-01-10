4e4439c90ae4c6bc21f19d44bb6807bba327e6f9
mxro <mxro@nowhere.com>
2015-01-10 11:52:12 +1100

    Adding services for monitoring services Autocommit Sat Jan 10 11:52:12 EST 2015

--- src/main/java/de/mxro/metrics/js/internal/JsMetricsNode.java
+++ src/main/java/de/mxro/metrics/js/internal/JsMetricsNode.java
@@ -6,6 +6,8 @@ import org.timepedia.exporter.client.NoExport;
 
 import de.mxro.metrics.MetricsCommon;
 import de.mxro.metrics.MetricsNode;
+import de.mxro.metrics.helpers.RecordOperation;
+import de.mxro.metrics.internal.operations.CounterEvent;
 
 @Export
 public class JsMetricsNode implements Exportable {
@@ -30,6 +32,19 @@ public class JsMetricsNode implements Exportable {
     @Export
     public void value(final String id, final int value) {
         this.metrics.record(MetricsCommon.value(id, value));
+    }
+
+    @Export
+    public void happened(final String id) {
+        this.metrics.record(MetricsCommon.happened(id));
+    }
+
+    public static RecordOperation increment(final String id) {
+        return new CounterEvent(1).setId(id);
+    }
+
+    public static RecordOperation decrement(final String id) {
+        return new CounterEvent(-1).setId(id);
     }
 
     @Export
