a3861d706bd8910dfaf3708d203d296f809054b2
mxro <mxro@nowhere.com>
2015-01-10 11:42:58 +1100

    a

---
+++ src/main/java/de/mxro/metrics/js/MetricsJs.java
@@ -0,0 +1,15 @@
+package de.mxro.metrics.js;
+
+import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.ExportPackage;
+import org.timepedia.exporter.client.Exportable;
+
+@ExportPackage("")
+@Export("Nextweb")
+public class MetricsJs implements Exportable {
+
+    public MetricsJs() {
+        super();
+    }
+
+}
